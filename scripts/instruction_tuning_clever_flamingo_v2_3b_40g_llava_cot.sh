5b9f3c46aa1f3ef510b59fcffb94bdfec3e46a0b
NgDMau <maunguyen.dinh@gmail.com>
2025-09-01 08:21:26 +0000

    Update tuning script

--- scripts/instruction_tuning_clever_flamingo_v2_3b_40g_llava_cot.sh
+++ scripts/instruction_tuning_clever_flamingo_v2_3b_40g_llava_cot.sh
@@ -11,10 +11,18 @@ export PYTHONPATH="$PYTHONPATH:open_flamingo"
 # export NCCL_DEBUG=INFO
 export NCCL_NVLS_ENABLE=0
 
+# Set variables
+BATCH_SIZE=256
+LR=2e-4
+EPOCHS=30
+LR_SCHEDULER="consine"
+
+RUN_NAME="runs/0829-clever_flamingo_v2_3b-batch${BATCH_SIZE}-lr${LR}-epochs${EPOCHS}-lrsched${LR_SCHEDULER}-resume-from-mpt7b"
+
 CUDA_VISIBLE_DEVICES='0,1,2,3,4,5,6,7' torchrun --nnodes=1 --nproc_per_node=8 --master_port=29502 open_flamingo/instruction_tuning/train.py \
     --instruction_data '/app/baseline_models/sample_data/llama_mri_cot/instruct_flamingo/dataset_config.json' \
     --instruction_prompt_templete 'guanaco-no-prompt' \
-    --run_name 'runs/0821-clever_flamingo_v2_3b-2k_context-40G-resume-from-mpt7b-checkpoint-03' \
+    --run_name "${RUN_NAME}" \
     --seed 42 \
     --vision_encoder_path 'ViT-L-14-336' \
     --lm_path 'anas-awadalla/mpt-7b' \
@@ -30,13 +38,13 @@ CUDA_VISIBLE_DEVICES='0,1,2,3,4,5,6,7' torchrun --nnodes=1 --nproc_per_node=8 --
     --skip_check_overlength \
     --train_num_samples 10000 \
     --epoch_num_samples 50 \
-    --batch_size 4 \
-    --learning_rate 1e-4 \
-    --gradient_accumulation_steps 4 \
+    --batch_size ${BATCH_SIZE} \
+    --learning_rate ${LR} \
+    --gradient_accumulation_steps 1 \
     --precision 'bf16' \
     --workers 8 \
-    --num_epochs 20 \
-    --lr_scheduler constant \
+    --num_epochs ${EPOCHS} \
+    --lr_scheduler ${LR_SCHEDULER} \
     --warmup_steps 10 \
     --logging_steps 1 \
 
